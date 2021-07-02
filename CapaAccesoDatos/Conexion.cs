927d7423760eaa6c1735122517c6df5da0a1cb77
Marco Castope <marcocastope@gmail.com>
2021-07-01 23:18:54 -0500

    cambio conexión

--- CapaAccesoDatos/Conexion.cs
+++ CapaAccesoDatos/Conexion.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data.SqlClient;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Data.SqlClient;
 
 namespace CapaAccesoDatos
 {
@@ -15,23 +10,18 @@ namespace CapaAccesoDatos
         private static readonly Conexion _instancia = new Conexion();
         public static Conexion Instancia
         {
-            get { return Conexion._instancia; }
+            get {
+                return Conexion._instancia;
+            }
         }
         #endregion singleton
         public SqlConnection Conectar()
         {
             SqlConnection cn = new SqlConnection();
-<<<<<<< HEAD
-<<<<<<< HEAD
-            cn.ConnectionString = "Data Source=D.; Initial Catalog = ProyectoHotel; Integrated Security=true";
-=======
-            cn.ConnectionString = "Data Source=DESKTOP-I8JI3BQ\\MSSQLSERVER01; Initial Catalog = ProyectoHotel; Integrated Security=true";
->>>>>>> origin/AnaCastillo
-            //cn.ConnectionString = @"Server=127.0.0.1,1433;Database=ProyectoHotel;User Id=SA;Password=Secret_123";
-=======
+
             //cn.ConnectionString = "Data Source=DESKTOP-I8JI3BQ\\MSSQLSERVER01; Initial Catalog = DB ProyectoHotel; Integrated Security=true";
             cn.ConnectionString = @"Server=127.0.0.1,1433;Database=ProyectoHotel;User Id=SA;Password=Secret_123";
->>>>>>> marcoluis
+
             return cn;
         }
 
